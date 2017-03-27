8f2da4c504a1d4e865682bd6866d6d2f3840e900
Justyna <kojustynka@gmail.com>
2017-03-27 23:11:54 +0200

    added function checkingConnection

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -32,3 +32,22 @@ function meInfo() {
 	
 	navigator.notification.alert(info);
 }
+
+/*FUNKCJA POLACZENIA Z SIECIA https://github.com/apache/cordova-plugin-network-information*/
+function checkingConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'Brak polaczenia';
+
+navigator.notification.alert(
+    'Typ polaczenia: ' + states[networkState]);
+}
+checkConnection();
